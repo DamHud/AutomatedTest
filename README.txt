3ea614a3037c7b0f18120ce72deacbcc4edef1e0
DamHud <hudzik.kontakt@gmail.com>
2023-05-02 17:29:26 +0200

    First commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+Test was created using tools:
+
+- selenium-java 4.9.0
+- maven 20
+- testng 7.7.1
+- testng reporting 1.2.2
+
+e-mail adress: automatedtest699@gmail.com
+password: mypassword123!?
+
+To launch test use AutomatedTest.java
+
+Reports are made in catalog C:\...\AutomatedTest\test-output\xml as a file AutomatedTest_results.xml
---
+++ src/test/java/AutomatedTest.java
@@ -0,0 +1,69 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class AutomatedTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.navigate().to("http://gmail.com/");
+    }
+
+    @Test
+    public void myFirstTest() throws InterruptedException {
+
+        driver.findElement(By.id("identifierId")).click();
+
+        driver.findElement(By.id("identifierId")).sendKeys("automatedtest699@gmail.com");
+
+        driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+
+        driver.findElement(By.name("Passwd")).click();
+
+        driver.findElement(By.name("Passwd")).sendKeys("mypassword123!?");
+
+        driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+
+        driver.findElement(By.cssSelector("div.T-I.T-I-KE.L3")).click();
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("div[role=presentation] input")).sendKeys("automatedtest699@gmail.com");
+
+        driver.findElement(By.xpath("(.//*[@aria-label='Message Body'])[2]")).click();
+
+        driver.findElement(By.xpath("(.//*[@aria-label='Message Body'])[2]")).sendKeys("This is an auto-generated mail");
+
+        driver.findElement(By.name("subjectbox")).click();
+
+        driver.findElement(By.name("subjectbox")).sendKeys("Test subject");
+
+        driver.findElement(By.cssSelector("div[data-tooltip=\"Send \u202A(Ctrl-Enter)\u202C\"]")).click();
+
+
+    }
+
+    @AfterMethod
+    public void afterTest() {
+
+        driver.close();
+
+        driver.quit();
+    }
+
+}
